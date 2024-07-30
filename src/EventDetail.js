3d31005c97ff0d737fa174ca83287999bb81af0a
Tunacodin <bostancibasituna58@gmail.com>
2024-07-30 14:51:43 +0300

    dateCard styled

--- src/EventDetail.js
+++ src/EventDetail.js
@@ -14,10 +14,50 @@ import { FontAwesome } from "@expo/vector-icons"; // İkonlar için
 const EventDetail = ({ route }) => {
   const { event } = route.params;
 
-  const baseURL = "https://kultursanat.izmir.bel.tr/Etkinlikler/";
+  const baseURL = "https://kultursanat.izmir.bel.tr/Etkinlikler";
+
+  // Tarih formatlama
+  const formatDate = (dateString) => {
+    if (!dateString) return "";
+    const date = new Date(dateString);
+    return `${date.getDate().toString().padStart(2, "0")} ${(
+      date.getMonth() + 1
+    )
+      .toString()
+      .padStart(2, "0")} ${date.getFullYear()}`;
+  };
+
+  // Saat formatlama
+  const formatTime = (dateString) => {
+    if (!dateString) return "";
+    const date = new Date(dateString);
+    return `${date.getHours().toString().padStart(2, "0")}:${date
+      .getMinutes()
+      .toString()
+      .padStart(2, "0")}`;
+  };
+
+  // Tarih karşılaştırma
+  const isSameDay = (startDate, endDate) => {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    return (
+      start.getFullYear() === end.getFullYear() &&
+      start.getMonth() === end.getMonth() &&
+      start.getDate() === end.getDate()
+    );
+  };
+
+  // HTML etiketlerini temizleme
+  const cleanText = (text) => {
+    if (!text) return "";
+    return text
+      .replace(/<\/?br>/g, "") // <br> etiketlerini kaldır
+      .replace(/[\r\n]+/g, " ") // Satır başlarını tek boşlukla değiştir
+      .replace(/&nbsp;/g, " "); // &nbsp; karakterlerini boşlukla değiştir
+  };
 
   const handleOpenEventPage = () => {
-    // Etkinlik URL'sini ana URL ile birleştir
     const eventURL = event.EtkinlikUrl;
     const url = `${baseURL}/${eventURL}`;
 
@@ -43,6 +83,9 @@ const EventDetail = ({ route }) => {
     }
   };
 
+  const eventStartDate = new Date(event.EtkinlikBaslamaTarihi);
+  const eventEndDate = new Date(event.EtkinlikBitisTarihi);
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollContainer}>
@@ -64,7 +107,9 @@ const EventDetail = ({ route }) => {
               color="white"
               style={styles.icon}
             />
-            <Text style={styles.description}>{event.KisaAciklama}</Text>
+            <Text style={styles.description}>
+              {cleanText(event.KisaAciklama)}
+            </Text>
           </View>
           <View style={styles.card}>
             <FontAwesome
@@ -76,25 +121,62 @@ const EventDetail = ({ route }) => {
             <Text style={styles.detailText}>{event.EtkinlikMerkezi}</Text>
           </View>
           <View style={styles.card}>
-            <FontAwesome
-              name="calendar"
-              size={24}
-              color="white"
-              style={styles.icon}
-            />
+
+
             <View style={styles.dateContainer}>
-              <Text style={styles.detailText}>
-                Başlangıç: {event.EtkinlikBaslamaTarihi}
-              </Text>
-              <Text style={styles.detailText}>
-                Bitiş: {event.EtkinlikBitisTarihi}
-              </Text>
+
+              <View
+                style={{
+                  display: "flex",
+                  justifyContent: "space-evenly",
+                  flexDirection: "row",
+                  alignItems: "center",
+
+                }}
+              >
+              
+                <FontAwesome
+                  name="calendar"
+                  size={24}
+                  color="white"
+                  style={styles.icon}
+                />
+                {isSameDay(
+                  event.EtkinlikBaslamaTarihi,
+                  event.EtkinlikBitisTarihi
+                ) ? (
+                  <Text style={styles.detailText}>
+                    Tarih: {formatDate(event.EtkinlikBaslamaTarihi)}
+                  </Text>
+                ) : (
+                  <>
+                    <View>
+                      <Text style={styles.detailText}>
+                        Başlangıç: {formatDate(event.EtkinlikBaslamaTarihi)}
+                      </Text>
+                      <Text style={styles.detailText}>
+                        Bitiş: {formatDate(event.EtkinlikBitisTarihi)}
+                      </Text>
+                    </View>
+                  </>
+                )}
+              </View>
+
+              <View style={styles.timeContainer}>
+                <Text style={styles.timeText}>
+                  {formatTime(event.EtkinlikBaslamaTarihi)}
+                </Text>
+                <Text style={styles.timeText}>
+                  {formatTime(event.EtkinlikBitisTarihi)}
+                </Text>
+              </View>
+
             </View>
           </View>
           <TouchableOpacity style={styles.button} onPress={handleOpenEventPage}>
             <Text style={styles.buttonText}>Etkinlik Sayfası</Text>
           </TouchableOpacity>
-          {event.BiletSatisLinki && ( // BiletSatisLinki varsa butonu göster
+          {event.BiletSatisLinki && (
             <TouchableOpacity style={styles.button} onPress={handleBuyTicket}>
               <Text style={styles.buttonText}>Bilet Satın Al</Text>
             </TouchableOpacity>
@@ -141,19 +223,35 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "bold",
     color: "#FFFFFF",
-    flexShrink: 1, // Title alanının taşmasını engeller
+    flexShrink: 1,
   },
   description: {
     fontSize: 16,
     color: "#CCCCCC",
-    flexShrink: 1, // Description alanının taşmasını engeller
+    flexShrink: 1,
   },
   detailText: {
     fontSize: 14,
     color: "#CCCCCC",
   },
   dateContainer: {
-    flexDirection: "column",
+    flexDirection: "row",
+ justifyContent: "space-between",
+    width: "100%",
+    alignItems: "center",
+  },
+  timeContainer: {
+    backgroundColor: "#2D2D2D", // Saat bilgileri için arka plan rengi
+    borderRadius: 8,
+ justifyContent:"space-between",
+    alignItems: "center",
+  },
+  timeText: {
+    fontSize: 14,
+    color: "#FFaaFF",
+    width: 50,
+    textAlign: "center",
+    padding: 2,
   },
   button: {
     backgroundColor: "#007BFF",
